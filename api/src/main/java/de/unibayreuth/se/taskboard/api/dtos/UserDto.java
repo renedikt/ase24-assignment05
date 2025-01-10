598c20c39435ff117f1f58b627a1a2a3a74b66b6
Benedikt Rief <rief.benedikt@gmail.com>
2025-01-10 01:56:57 +0100

    Add constraints to username in UserDto

--- api/src/main/java/de/unibayreuth/se/taskboard/api/dtos/UserDto.java
+++ api/src/main/java/de/unibayreuth/se/taskboard/api/dtos/UserDto.java
@@ -1,6 +1,8 @@
 package de.unibayreuth.se.taskboard.api.dtos;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import org.springframework.lang.Nullable;
 
@@ -17,5 +19,7 @@ public class UserDto {
         @Nullable
         private LocalDateTime createdAt;
         @NotNull
+        @NotBlank
+        @Size(max = 255, message = "Name can be at most 255 characters long.")
         private String name;
 }
